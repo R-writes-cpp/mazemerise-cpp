93c4ec1d8e27f2b5c1b9218a1bfdd4a4464a380c
R Alam <159417475+R-writes-cpp@users.noreply.github.com>
2024-02-29 12:47:10 +0000

    Streamline existing codebase

--- src/generate.cpp
+++ src/generate.cpp
@@ -14,26 +14,21 @@ enum colour {black = 0, white = 2}; // enum that matches the .pgm format - here 
 
 struct point_data {
     bool unvisited = true;
-    int neighbours = 0, colour = black;
+    int neighbours = 0,
+        colour = black;
 };
 
-int main(int argc, char** argv) { // input order: output file directory, width of desired maze, height of desired maze
-    const int width = atoi(argv[2]), height = atoi(argv[3]);
-
-    if (width <= 0 || height <= 0) {
-        cerr << "Error: dimensions entered are non-positive. Please enter positive dimensions.\n";
-        return 0;
-    }
-
-    srand(time(0)); // since we'll only require integer random values and we're not concerned with slightly skewed distributions, it suffices to use the rand() function from the C standard library instead of anything from the C++11 <random> header.
-
-    vector<vector<point_data>> lookup (height, vector<point_data>(width));
-
+void generate_maze(vector<vector<point_data>>& lookup, const size_t& width, const size_t& height) {
     {
+        bool backtracking = false; // in backtracking mode, our requirements for moving to the next pixel change. we'll permit moving to previously visited pixels in backtracking mode, assuming there aren't any neighbours to it excluding the current neighbour to it which is in our stack. backtracking mode allows us to go back and add more paths to our maze, which is essential.
+
+        auto check_point = [&](int y, int x, vector<point>& possible_points) { // checks whether the point (x, y) on the image can be moved to
+            if (lookup[y][x].neighbours++ == (backtracking ? 1 : 0) && (backtracking || lookup[y][x].unvisited))
+                possible_points.push_back({x, y});
+        };
+
         stack<point> s;
         s.push({0, 0});
-
-        bool backtracking = false; // in backtracking mode, our requirements for moving to the next pixel change. we'll permit moving to previously visited pixels in backtracking mode, assuming there aren't any neighbours to it excluding the current neighbour to it which is in our stack. backtracking mode allows us to go back and add more paths to our maze, which is essential.
 
         while (!s.empty()) {
             auto& x = s.top().x,
@@ -48,24 +43,21 @@ int main(int argc, char** argv) { // input order: output file directory, width o
             vector<point> possible_points {};
             possible_points.reserve(4);
 
-            if (x != 0 && lookup[y][x-1].neighbours++ == (backtracking ? 1 : 0) && (backtracking || lookup[y][x-1].unvisited)) // the order here matters. first of all, we need to make sure x != 0 so that the program doesn't segfault when we access the left neighbour pixel. second of all we need to increment the neighbours count of the current pixel, assuming it exists, then check if we're either backtracking or that the left neighbour pixel is unvisited. if we do the unvisited check before the neighbours check we won't increment the neighbours counter, which leads to invalid mazes.
-                possible_points.push_back({x-1, y});
+            {
+                if (x != 0)
+                    check_point (y, x-1, possible_points);
+                if (y != 0)
+                    check_point (y-1, x, possible_points);
+                if (x != width - 1)
+                    check_point (y, x+1, possible_points);
+                if (y != height - 1)
+                    check_point (y+1, x, possible_points);
+            }
 
-            if (y != 0 && lookup[y-1][x].neighbours++ == (backtracking ? 1 : 0) && (backtracking || lookup[y-1][x].unvisited))
-                possible_points.push_back({x, y-1});
-
-            if (x != width - 1 && lookup[y][x+1].neighbours++ == (backtracking ? 1 : 0) && (backtracking || lookup[y][x+1].unvisited))
-                possible_points.push_back({x+1, y});
-
-            if (y != height - 1 && lookup[y+1][x].neighbours++ == (backtracking ? 1 : 0) && (backtracking || lookup[y+1][x].unvisited))
-                possible_points.push_back({x, y+1});
-
-            if (possible_points.size()) { // if there's directions we can continue in...
-                auto& next_point = possible_points[rand() % possible_points.size()]; // select a random point out of our options
-                s.push(next_point), // push it onto the stack, where it'll be the next point we visit
-                lookup[next_point.y][next_point.x].unvisited = false,
+            if (possible_points.size()) // if there's directions we can continue in...
+                s.push(possible_points[rand() % possible_points.size()]), // push a random element in the possible_points vector to the stack
                 backtracking = false; // disable backtracking mode
-            } else // if we're at a dead end (i.e. there's no pixels we can move to) ...
+            else // if we're at a dead end (i.e. there's no pixels we can move to) ...
                 s.pop(),
                 backtracking = true; // enable backtracking mode to go back through the stack
         }
@@ -76,14 +68,20 @@ int main(int argc, char** argv) { // input order: output file directory, width o
         for (size_t y = height - 1, x = width - 1; x && y && lookup[y][x].colour == black; move_y ? --y : --x) // we'll "tunnel through" either the pixels above it or the pixels to the left of it so that we can reach the end
             lookup[y][x].colour = white;
     }
+}
 
-    ofstream img (argv[1]);
+void output (char*& filename, vector<vector<point_data>>& lookup, const size_t& width, const size_t& height) {
+    ofstream img (filename);
 
     img << "P2\n" << width + 2 << ' ' << height + 2 << "\n2\n"; // we're adding 2 to each of the dimensions as part of our black border
 
     img << black << ' ' << white << ' '; // set up the black border on top, ensuring that the top-left pixel has an entrance above it
-    for (int i = 0; i < width; ++i)
-        img << black << ' ';
+    auto vertical_border = [&width, &img]() { // lambda which sets a vertical border
+        for (size_t i = 0; i < width; ++i)
+            img << black << ' ';
+    };
+
+    vertical_border();
     img << '\n';
 
     for (auto& r : lookup) {
@@ -93,7 +91,22 @@ int main(int argc, char** argv) { // input order: output file directory, width o
         img << black << '\n';
     }
 
-    for (int i = 0; i < width; ++i) // set up the black border on the bottom, ensuring that the bottom-right pixel has an exit below it
-        img << black << ' ';
+    vertical_border();
     img << white << ' ' << black;
+}
+
+int main(int argc, char** argv) { // input order: output file directory, width of desired maze, height of desired maze
+    if (argc != 4) {
+        cerr << "Error: bad number of inputs. Please only input an output .pgm filename and the dimensions of the output maze.\n";
+        return 1;
+    }
+
+    const size_t width = atoi(argv[2]), height = atoi(argv[3]);
+
+    srand(time(0)); // since we'll only require integer random values and we're not concerned with slightly skewed distributions, it suffices to use the rand() function from the C standard library instead of anything from the C++11 <random> header.
+
+    vector<vector<point_data>> lookup (height, vector<point_data>(width));
+
+    generate_maze(lookup, width, height);
+    output (argv[1], lookup, width, height);
 }
--- src/solve.cpp
+++ src/solve.cpp
@@ -14,99 +14,103 @@ struct point {
 enum colour {black = 0, grey = 1, white = 2}; // we'll be using the .pgm format here - black pixels represent maze walls, white pixels represent potential paths, and grey pixels represent the path from the start to the end of the maze
 
 struct point_data {
-    bool leads_to_dead_end = false;
+    bool no_dead_end = true;
     int colour = black;
 };
 
-int main(int argc, char** argv) { // input order: input file directory, output file directory
-    vector<vector<point_data>> lookup;
-    int width, height; // these represent the width and height of the maze, excluding its border
+void input (char*& filename, vector<vector<point_data>>& lookup, size_t& width, size_t& height) {
+    ifstream img (filename); // input .pgm image
+    string str;
+    getline (img, str), getline (img, str); // skips the first line "P2" and moves onto the second line with the dimensions of the image
 
-    {
-        ifstream img (argv[1]); // input .pgm image
-        string str;
-        getline (img, str), getline (img, str); // skips the first line "P2" and moves onto the second line with the dimensions of the image
-
-        try {
-            int img_width; // represents the width of the original image, including its border. a variable different from "width" is used for clarity.
-            size_t i = str.find_first_of(' ');
-            img_width = stoi (str, &i);
-            str.reserve (2 * img_width); // max length of each row from now on will be 2 times the width (including the spacing between each pixel and the line break)
-
-            width = img_width - 2,
-            height = stoi (string (str.begin() + i + 1, str.end())) - 2;
-        }
-
-        catch (...) { // if there's any errors with stoi...
-            cerr << "Error: invalid input.\n";
-            return 0;
-        }
-
-        getline (img, str), getline (img, str), getline (img, str); // skips the number of shades (which is already known assuming a valid input has been provided) and the border, then moves onto the first row with unique data
-
-        lookup = vector<vector<point_data>> (height, vector<point_data> (width));
-        for (int y = 0; y < height; ++y, getline (img, str))
-            for (int x = 0; x < width; ++x)
-                if (auto current = str[2 * (x + 1)] - '0'; current != 0 && current != 2) { // input images must only consist of 0s and 2s (and spaces, but we're skipping over those with 2 * (x + 1))
-                    cerr << "Error: invalid input.\n";
-                    return 0;
-                } else
-                    lookup[y][x].colour = current;
-    }
-
-    if (lookup[0][0].colour == black || lookup[height - 1][width - 1].colour == black) {
-        cerr << "Error: invalid maze provided.\n";
-        return 0;
-    }
-
-    {
-        bool path_found = false;
+    try {
+        size_t i = str.find_first_of(' ');
 
         {
-            stack<point> s; // stack which uses a standard depth-first search algorithm for maze solving
-            s.push({0, 0});
+            int img_width = stoi (str, &i); // represents the width of the original image, including its border. a variable different from "width" is used for clarity.
+            str.reserve (2 * img_width); // max length of each row from now on will be 2 times the width (including the spacing between each pixel and the line break)
 
-            while (s.size()) {
-                auto& x = s.top().x,
-                    y = s.top().y;
-                auto& current = lookup[y][x];
-
-                if (x == width - 1 && y == height - 1) { // i.e. "have we reached the end of the maze?"
-                    current.colour = grey,
-                    path_found = true;
-                    break;
-                }
-
-                // NOTE: on average, it is significantly faster to prioritise going right and going down instead of going left and going up - this is because these directions "bring us closer" to the end point (i.e. they decrease the Euclidean distance between the next point and the end point), and when we're closer to the end, we're generally more likely to be on an end-reaching path
-                auto next_point = (x != width - 1 && lookup[y][x+1].colour == white && !lookup[y][x+1].leads_to_dead_end)  ? point {x+1, y} :
-                                (y != height - 1 && lookup[y+1][x].colour == white && !lookup[y+1][x].leads_to_dead_end) ? point {x, y+1} :
-                                (x != 0 && lookup[y][x-1].colour == white && !lookup[y][x-1].leads_to_dead_end)          ? point {x-1, y} :
-                                (y != 0 && lookup[y-1][x].colour == white && !lookup[y-1][x].leads_to_dead_end)          ? point {x, y-1} :
-                                point {-1, -1}; // final value assuming no there are no available neighbours
-
-                if (next_point.x == -1 && next_point.y == -1) // if there are no available neighbours...
-                    current.colour = white, // then the current point isn't part of our path to the end and should be set to white
-                    current.leads_to_dead_end = true,
-                    s.pop();
-                else // if there are available neighbours...
-                    current.colour = grey, // then the current point is part of our path to the end (for now)
-                    s.push(next_point);
-            }
+            width = img_width - 2;
         }
 
-        if (!path_found) {
-            cout << "No path found.\n";
-            return 0;
+        height = stoi (string (str.begin() + i + 1, str.end())) - 2;
+    }
+
+    catch (...) { // if there's any errors with stoi...
+        cerr << "Error: invalid input. Have you entered a valid .pgm file?\n";
+        exit(1);
+    }
+
+    getline (img, str), getline (img, str), getline (img, str); // skips the number of shades (which is already known assuming a valid input has been provided) and the border, then moves onto the first row with unique data
+
+    lookup = vector<vector<point_data>> (height, vector<point_data> (width));
+    for (size_t y = 0; y < height; ++y, getline (img, str))
+        for (size_t x = 0; x < width; ++x)
+            if (auto current = str[2 * (x + 1)] - '0'; current != 0 && current != 2) { // input images must only consist of 0s and 2s (and spaces, but we're skipping over those with 2 * (x + 1))
+                cerr << "Error: invalid input. Have you entered a valid .pgm file?\n";
+                exit(1);
+            } else
+                lookup[y][x].colour = current;
+}
+
+void solve (vector<vector<point_data>>& lookup, size_t& width, size_t& height) {
+    if (lookup[0][0].colour == black || lookup[height - 1][width - 1].colour == black) {
+        cout << "No path found.\n";
+        exit(2);
+    }
+
+    bool path_found = false;
+
+    {
+        stack<point> s; // stack which uses a standard depth-first search algorithm for maze solving
+        s.push({0, 0});
+
+        while (s.size()) {
+            auto& x = s.top().x,
+                  y = s.top().y;
+            auto& current = lookup[y][x];
+
+            if (x == width - 1 && y == height - 1) { // i.e. "have we reached the end of the maze?"
+                current.colour = grey,
+                path_found = true;
+                break;
+            }
+
+            // NOTE: on average, it is significantly faster to prioritise going right and going down instead of going left and going up - this is because these directions "bring us closer" to the end point (i.e. they decrease the Euclidean distance between the next point and the end point), and when we're closer to the end, we're generally more likely to be on an end-reaching path
+            auto next_point = (x != width - 1 && lookup[y][x+1].colour == white && lookup[y][x+1].no_dead_end)  ? point {x+1, y} :
+                              (y != height - 1 && lookup[y+1][x].colour == white && lookup[y+1][x].no_dead_end) ? point {x, y+1} :
+                              (x != 0 && lookup[y][x-1].colour == white && lookup[y][x-1].no_dead_end)          ? point {x-1, y} :
+                              (y != 0 && lookup[y-1][x].colour == white && lookup[y-1][x].no_dead_end)          ? point {x, y-1} :
+                              point {-1, -1}; // final value assuming no there are no available neighbours
+
+            if (next_point.x == -1 && next_point.y == -1) // if there are no available neighbours...
+                current.colour = white, // then the current point isn't part of our path to the end and should be set to white
+                current.no_dead_end = false,
+                s.pop();
+            else // if there are available neighbours...
+                current.colour = grey, // then the current point is part of our path to the end (for now)
+                s.push(next_point);
         }
     }
 
-    ofstream img (argv[2]); // output image
+    if (!path_found) {
+        cout << "No path found.\n";
+        exit(2);
+    }
+}
+
+void output (char*& filename, vector<vector<point_data>>& lookup, const size_t& width, const size_t& height) {
+    ofstream img (filename); // output image
 
     img << "P2\n" << width + 2 << ' ' << height + 2 << "\n2\n"; // 3 different greyscale values means we have black, grey and white (represented by 0, 1 and 2 respectively). this is all we need.
 
     img << black << ' ' << grey << ' '; // set up the black border on top, ensuring that the top-left pixel has an entrance above it
-    for (int i = 0; i < width; ++i)
-        img << black << ' ';
+    auto vertical_border = [&width, &img]() { // lambda which sets a vertical border
+        for (size_t i = 0; i < width; ++i)
+            img << black << ' ';
+    };
+
+    vertical_border();
     img << '\n';
 
     for (auto& r : lookup) {
@@ -116,7 +120,22 @@ int main(int argc, char** argv) { // input order: input file directory, output f
         img << black << '\n';
     }
 
-    for (int i = 0; i < width; ++i) // set up the black border on the bottom, ensuring that the bottom-right pixel has an exit below it
-        img << black << ' ';
+    vertical_border();
     img << grey << ' ' << black;
+}
+
+int main(int argc, char** argv) { // input order: input file directory, output file directory
+    if (argc != 3) {
+        cerr << "Error: bad number of inputs. Please only input an input .pgm filename and an output .pgm filename.\n";
+        return 1;
+    }
+
+    vector<vector<point_data>> lookup;
+    size_t width, height; // these represent the width and height of the maze, excluding its border
+
+    input (argv[1], lookup, width, height);
+
+    solve (lookup, width, height);
+
+    output(argv[2], lookup, width, height);
 }
